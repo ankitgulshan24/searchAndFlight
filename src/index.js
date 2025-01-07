6d734d1e4dcc3f8d946e8af7bd13e11bb1252b2c
Ankit <ankitkrnwd8@gmail.com>
2025-01-07 23:48:38 +0530

    setting up the middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 const express= require("express");
+const bodyParser= require("body-parser");
+
+
+
 const {PORT}= require('./config/serverConfig')
 require('dotenv').config()
 const setupAndStartServer= async()=>{
     // create a express object 
     const app= express();
+
+    // setting the middleware 
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.use((req, res, nes)=>{
+
+    })
+
+
     app.listen(PORT, ()=>{
         console.log(`server stared at ${PORT}`);
         // console.log(process.env);
